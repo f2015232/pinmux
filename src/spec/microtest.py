2abadd1ccd9a7f7817be63e716a48cd226292d4c
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2018-06-24 12:09:20 +0100

    reduce microtest further in size

--- src/spec/microtest.py
+++ src/spec/microtest.py
@@ -50,11 +50,11 @@ def pinspec(of):
     ps = PinSpec(pinbanks, fixedpins, function_names)
 
     # Bank A, 0-3
-    ps.gpio("", ('A', 0), 0, 0, 6)
+    ps.gpio("", ('A', 0), 0, 0, 3)
     ps.uart("0", ('A', 0), 1)
-    ps.uart("1", ('A', 2), 1)
+    #ps.uart("1", ('A', 2), 1)
     ps.i2c("0", ('A', 1), 2)
-    ps.i2c("1", ('A', 2), 4)
+    #ps.i2c("1", ('A', 2), 4)
 
     microtest = ['UART0', 'TWI0', ]
     microtest_eint = []
