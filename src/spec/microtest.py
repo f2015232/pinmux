431ee33e813d4d28608e7efb1b23ff1d969c238d
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2018-06-25 08:13:07 +0100

    correct microtest spec, single interface must take out numbering

--- src/spec/microtest.py
+++ src/spec/microtest.py
@@ -51,12 +51,14 @@ def pinspec(of):
 
     # Bank A, 0-3
     ps.gpio("", ('A', 0), 0, 0, 3)
-    ps.uart("0", ('A', 0), 1)
+    ps.uart("", ('A', 0), 1)
+    #ps.uart("0", ('A', 0), 1)
     #ps.uart("1", ('A', 2), 1)
-    ps.i2c("0", ('A', 1), 2)
+    ps.i2c("", ('A', 1), 2)
+    #ps.i2c("0", ('A', 1), 2)
     #ps.i2c("1", ('A', 2), 4)
 
-    microtest = ['UART0', 'TWI0', ]
+    microtest = ['UART', 'TWI', ]
     microtest_eint = []
     microtest_pwm = []
     descriptions = {
